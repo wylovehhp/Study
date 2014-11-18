78f564d2a736f63c4a0e58cb1567da27d9b35590
wyd <wyd@192.168.2.105>
2014-11-18 22:59:12 +0800

    by 王煜 内容：第一次修改

---
+++ src/FirstTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author 王小红 E-mail:493026465@qq.com
+ * @version 创建时间：2014年11月18日 下午10:56:35 类说明
+ */
+public class FirstTest {
+	public static void main(String[] args) {
+		System.out.println("老婆大傻叉");
+	}
+}
